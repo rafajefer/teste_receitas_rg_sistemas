cfd95e817cdfc5a37cdf259c45a883173380a3f9
Rafael jerson pena <rafa.jefer@gmail.com>
2025-11-26 13:19:18 -0300

    test: criando o teste unitario da classe SanctumTokenService

---
+++ src/tests/Unit/Infrastructure/Services/Token/SanctumTokenServiceTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Unit\Infrastructure\Services\Token;
+
+use App\Infrastructure\Services\Token\SanctumTokenService;
+use App\Infrastructure\Persistence\Eloquent\Models\UserModel;
+use PHPUnit\Framework\TestCase;
+use Mockery;
+
+class SanctumTokenServiceTest extends TestCase
+{
+    public function tearDown(): void
+    {
+        Mockery::close();
+        parent::tearDown();
+    }
+
+    public function test_generate_returns_token_string(): void
+    {
+        $userId = 1;
+        $userMock = Mockery::mock();
+        $userMock->shouldReceive('createToken')
+            ->with('auth')
+            ->andReturn((object)['plainTextToken' => 'token123']);
+
+        $userModelMock = Mockery::mock(UserModel::class);
+        $userModelMock->shouldReceive('find')
+            ->with($userId)
+            ->andReturn($userMock);
+
+        $service = new SanctumTokenService($userModelMock);
+        $token = $service->generate($userId);
+
+        $this->assertEquals('token123', $token);
+    }
+}
