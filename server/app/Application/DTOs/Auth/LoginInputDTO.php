6e12b783c59e9e695f2ee45f075da3d222ce6893
Rafael jerson pena <rafa.jefer@gmail.com>
2025-11-27 12:44:58 -0300

    refactor: adicionando readonly nas propriedades da classe LoginInputDTO

--- server/app/Application/DTOs/Auth/LoginInputDTO.php
+++ server/app/Application/DTOs/Auth/LoginInputDTO.php
@@ -5,7 +5,7 @@ namespace App\Application\DTOs\Auth;
 final class LoginInputDTO
 {
     public function __construct(
-        public string $login,
-        public string $password,
+        public readonly string $login,
+        public readonly string $password,
     ) {}
 }
