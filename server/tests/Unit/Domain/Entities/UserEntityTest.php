d9f5a7de57110e558345863f5a0fb8fd8bd05fb7
Rafael jerson pena <rafa.jefer@gmail.com>
2025-11-29 14:01:06 -0300

    test: corrigindo o teste unitario da classe UserEntity

--- server/tests/Unit/Domain/Entities/UserEntityTest.php
+++ server/tests/Unit/Domain/Entities/UserEntityTest.php
@@ -10,13 +10,13 @@ class UserEntityTest extends TestCase
     public function test_user_entity_properties_are_set_correctly(): void
     {
         $user = new UserEntity(
-            id: 1,
+            id: '1',
             name: 'Rafael',
             login: 'rafael123',
             password: 'senhaSegura'
         );
 
-        $this->assertEquals(1, $user->id);
+        $this->assertEquals('1', $user->id);
         $this->assertEquals('Rafael', $user->name);
         $this->assertEquals('rafael123', $user->login);
         $this->assertEquals('senhaSegura', $user->password);
@@ -25,13 +25,13 @@ class UserEntityTest extends TestCase
     public function test_user_entity_accepts_null_id(): void
     {
         $user = new UserEntity(
-            id: null,
+            id: '',
             name: 'Rafael',
             login: 'rafael123',
             password: 'senhaSegura'
         );
 
-        $this->assertNull($user->id);
+        $this->assertEquals('', $user->id);
         $this->assertEquals('Rafael', $user->name);
         $this->assertEquals('rafael123', $user->login);
         $this->assertEquals('senhaSegura', $user->password);
