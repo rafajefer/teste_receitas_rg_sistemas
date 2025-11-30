0c448bd2b5a60275d75308320a9b0a57efd648d7
Rafael jerson pena <rafa.jefer@gmail.com>
2025-11-30 18:30:41 -0300

    feat: sempre que o backend retorna erro 401 usuario é redirecionado para login

--- client/src/main.js
+++ client/src/main.js
@@ -12,4 +12,17 @@ if (token) {
 	axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
 }
 
+// Intercepta respostas 401 e redireciona para login
+axios.interceptors.response.use(
+	response => response,
+	error => {
+		if (error.response && error.response.status === 401) {
+			localStorage.removeItem('token')
+			localStorage.removeItem('user')
+			window.location.href = '/login'
+		}
+		return Promise.reject(error)
+	}
+)
+
 createApp(App).use(store).use(router).mount('#app')
