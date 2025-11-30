83a699c10a6a66e8fb2f24b7515a337776943862
Rafael jerson pena <rafa.jefer@gmail.com>
2025-11-30 18:44:57 -0300

    feat: ajustes na interface PdfGenerationServiceInterface

--- server/app/Domain/Services/PdfGenerator/PdfGenerationServiceInterface.php
+++ server/app/Domain/Services/PdfGenerator/PdfGenerationServiceInterface.php
@@ -2,12 +2,12 @@
 
 namespace App\Domain\Services\PdfGenerator;
 
-use App\Application\DTOs\Report\PdfFileDTO;
+use App\Application\DTOs\Report\PdfFileOutputDTO;
 
 interface PdfGenerationServiceInterface
 {
     /**
      * Gera o PDF e retorna o conteúdo binário e nome do arquivo.
      */
-    public function generate(array $data, string $view, string $filename): PdfFileDTO;
+    public function generate(array $data, string $view, string $filename): PdfFileOutputDTO;
 }
