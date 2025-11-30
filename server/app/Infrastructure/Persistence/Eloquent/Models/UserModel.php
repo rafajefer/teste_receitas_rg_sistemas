6559873af16efd06c6d003cd41c48ef66dfabb68
Rafael jerson pena <rafa.jefer@gmail.com>
2025-11-30 18:52:05 -0300

    feat: alterando autenticacao na userModel

--- server/app/Infrastructure/Persistence/Eloquent/Models/UserModel.php
+++ server/app/Infrastructure/Persistence/Eloquent/Models/UserModel.php
@@ -3,11 +3,11 @@
 namespace App\Infrastructure\Persistence\Eloquent\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Laravel\Sanctum\HasApiTokens;
 
-class UserModel extends Model
+class UserModel extends Authenticatable
 {
     use HasFactory, HasApiTokens;
 
