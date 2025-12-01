37868163b034df78106231ddbbb2daff77cd96eb
Rafael jerson pena <rafa.jefer@gmail.com>
2025-12-01 14:26:11 -0300

    feat: colocando password_confirmation na validacao do RegisterRequest

--- server/app/Interfaces/Http/Requests/Auth/RegisterRequest.php
+++ server/app/Interfaces/Http/Requests/Auth/RegisterRequest.php
@@ -17,6 +17,7 @@ class RegisterRequest extends FormRequest
             'name' => 'required|string|max:100',
             'login' => 'required|string|max:100|unique:usuarios,login',
             'password' => 'required|string|min:6',
+            'password_confirmation' => 'required|string|same:password',
         ];
     }
 }
