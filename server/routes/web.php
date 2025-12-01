c6cb547cc29c9047772bd6c63039a0b1ccc8c803
Rafael jerson pena <rafa.jefer@gmail.com>
2025-12-01 15:43:36 -0300

    feat: alterando a resposta do acesso web do laravel

--- server/routes/web.php
+++ server/routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return response()->json(['message' => 'API is running access /api']);
+    return response()->json(['message' => 'API is running access /api/pong']);
 });
